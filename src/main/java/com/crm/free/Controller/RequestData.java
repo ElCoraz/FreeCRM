9ae48ecb62a9903eb82dbbd34228c2c3f2b40bce
ElCoraz <dmwhite19850908@gmail.com>
2024-04-17 22:57:42 +0500

    rest

---
+++ src/main/java/com/crm/free/Controller/RequestData.java
@@ -0,0 +1,31 @@
+package com.crm.free.Controller;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class RequestData {
+
+	public String[] path;
+	public HashMap<String, String> parametrs;
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	public RequestData(HttpServletRequest request) {
+
+		this.path = request.getRequestURI().split("/");
+		this.path = Arrays.copyOfRange(this.path, 1, this.path.length);
+
+		String[] query = request.getQueryString().split("&");
+
+		this.parametrs = new HashMap();
+
+		for(int i = 0; i < query.length; i++) {
+			String[] parametr = query[i].split("=");
+
+			this.parametrs.put(parametr[0], parametr[1]);
+		}
+
+    }
+    
+}
--- src/main/java/com/crm/free/Controller/RestApiController.java
+++ src/main/java/com/crm/free/Controller/RestApiController.java
@@ -1,9 +1,14 @@
 package com.crm.free.Controller;
 
 import org.json.JSONObject;
-
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,29 +16,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/api")
 public class RestApiController {
 
-	@RequestMapping(value = "/getName", produces="application/json; charset=UTF-8", method = RequestMethod.GET)
-	public String index1() {
+	@RequestMapping(value = "/getName", 
+	produces = MediaType.APPLICATION_JSON_VALUE, 
+	method = RequestMethod.GET)
+	public ResponseEntity<String> getName_GET(HttpServletRequest request) {
+
+		RequestData requestData = new RequestData(request);
+
 		JSONObject jsonObject = new JSONObject();
 
-		jsonObject.put("value", "new name");
+		jsonObject.put("value", "new name1");
 
-		return jsonObject.toString();
+		return ResponseEntity.ok(jsonObject.toString());
 	}
 
-	@RequestMapping(value = "/getName", produces = "application/json; charset=UTF-8", method = RequestMethod.POST)
-	public String index2() {
+	@RequestMapping(value = "/getName", 
+	produces = MediaType.APPLICATION_JSON_VALUE, 
+	method = RequestMethod.POST)
+	public ResponseEntity<String> getName_POST(HttpServletRequest request) {
+
+		RequestData requestData = new RequestData(request);
+
 		JSONObject jsonObject = new JSONObject();
 
-		jsonObject.put("value", "new name");
+		jsonObject.put("value", "new name2");
 
-		return jsonObject.toString();
+		return ResponseEntity.ok(jsonObject.toString());
 	}
+
+	@RequestMapping(value = "/getName", 
+	produces = MediaType.APPLICATION_JSON_VALUE, 
+	consumes = MediaType.APPLICATION_JSON_VALUE,
+	method = RequestMethod.POST)
+	public ResponseEntity<String> getName_POST(@Validated @RequestBody String body, HttpServletRequest request) {
+ 
+		RequestData requestData = new RequestData(request);
 	
+		JSONObject jsonObject = new JSONObject();
 
-	/*
-	@PostMapping("/getName")
-	public String index() {
-		return "Hello";
+        jsonObject.put("value", "new name3");
+
+		return ResponseEntity.ok(jsonObject.toString());
 	}
-	*/
 }
